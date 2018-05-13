1ea9adccbe4f8d53b417193a46be6406019ad895
Andre Arko <andre@arko.net>
2018-05-13 01:23:50 -0700

    use uuids as primary keys in postgres

--- config/application.rb
+++ config/application.rb
@@ -19,15 +19,11 @@ Bundler.require(*Rails.groups)
 
 module Ecosystem
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
   end
 end
