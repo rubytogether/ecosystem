81668b25629da57478ef76d043135ca06fb76738
Andre Arko <andre@arko.net>
2018-09-08 23:11:28 -0700

    table to track parsed logs later

---
+++ db/migrate/20180909060930_create_parsed_logs.rb
@@ -0,0 +1,9 @@
+class CreateParsedLogs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :parsed_logs, id: :uuid do |t|
+      t.string :filename
+      t.timestamp :parsed_at
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_13_075308) do
+ActiveRecord::Schema.define(version: 2018_09_09_060930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "parsed_logs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "filename"
+    t.datetime "parsed_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stats", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.date "date"
