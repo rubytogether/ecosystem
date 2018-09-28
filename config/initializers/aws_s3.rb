5e9fdcac6745fbfbffd184498a488f97c75dc7b9
Andre Arko <andre@arko.net>
2018-09-28 18:03:11 -0500

    region is set by AWS_REGION now

--- config/initializers/aws_s3.rb
+++ config/initializers/aws_s3.rb
@@ -1,9 +1,6 @@
 require "aws-sdk-core"
 
-Aws.config.update({
-  region: "us-west-2",
-  logger: ::Rails.logger
-})
+Aws.config.update(logger: ::Rails.logger)
 
 Rails.application.config.stats = OpenStruct.new(
   bucket_name: ENV.fetch("STATS_BUCKET_NAME", "rubygems-logs-staging.rubytogether"),
