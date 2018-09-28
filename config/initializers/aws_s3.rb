864ec7e4b94c5d1080368a047553712f8806b74a
Andre Arko <andre@arko.net>
2018-09-28 17:00:14 -0500

    allow accessing stats config via methods

--- config/initializers/aws_s3.rb
+++ config/initializers/aws_s3.rb
@@ -5,7 +5,7 @@ Aws.config.update({
   logger: ::Rails.logger
 })
 
-Rails.application.config.stats = {
+Rails.application.config.stats = OpenStruct.new(
   bucket_name: ENV.fetch("STATS_BUCKET_NAME", "rubygems-logs-staging.rubytogether"),
   prefix: ENV.fetch("STATS_BUCKET_PREFIX", "fastly_json/")
-}
+)
