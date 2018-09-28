0a9eeee4863c2a1623a431a54cea23c5cbe96c79
Andre Arko <andre@arko.net>
2018-09-28 18:03:25 -0500

    omg the stats are in fastly_stats/ 🤦🏻‍♂️

--- config/initializers/aws_s3.rb
+++ config/initializers/aws_s3.rb
@@ -4,5 +4,5 @@ Aws.config.update(logger: ::Rails.logger)
 
 Rails.application.config.stats = OpenStruct.new(
   bucket_name: ENV.fetch("STATS_BUCKET_NAME", "rubygems-logs-staging.rubytogether"),
-  prefix: ENV.fetch("STATS_BUCKET_PREFIX", "fastly_json/")
+  prefix: ENV.fetch("STATS_BUCKET_PREFIX", "fastly_stats/")
 )
