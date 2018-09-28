c97368130df834047f88a46a6a922d7f00a70c31
Andre Arko <andre@arko.net>
2018-09-28 16:25:53 -0500

    configure S3 buckets via env vars

--- config/initializers/aws_s3.rb
+++ config/initializers/aws_s3.rb
@@ -4,3 +4,8 @@ Aws.config.update({
   region: "us-west-2",
   logger: ::Rails.logger
 })
+
+Rails.application.config.stats = {
+  bucket_name: ENV.fetch("STATS_BUCKET_NAME", "rubygems-logs-staging.rubytogether"),
+  prefix: ENV.fetch("STATS_BUCKET_PREFIX", "fastly_json/")
+}
