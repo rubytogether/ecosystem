ae5ddcab74aa4d6fb2003cc9196818ad7aba1850
Andre Arko <andre@arko.net>
2018-09-30 20:46:22 -0500

    remove unused table from schema

--- db/schema.rb
+++ db/schema.rb
@@ -26,13 +26,6 @@ ActiveRecord::Schema.define(version: 2018_09_29_063650) do
     t.index ["key"], name: "index_import_statuses_on_key"
   end
 
-  create_table "parsed_logs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "filename", null: false
-    t.datetime "parsed_at", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "stats", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.date "date", null: false
     t.string "key", null: false
