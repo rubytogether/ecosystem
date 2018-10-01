94b179316bceee7a74225c5f693674a92cc53556
Andre Arko <andre@arko.net>
2018-10-01 00:22:40 -0700

    maybe this will fix the prod JS error?
    it seems like the webpack minification of the non-minified rickshaw is
    causing some single letter variable to get shadowed and everything to
    break. presumably the already-minified version will not have that
    problem? hopefully?

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 import "rickshaw/rickshaw.min.css";
 import "stylesheets/application.css";
 
-import Rickshaw from "rickshaw/rickshaw.js";
+import Rickshaw from "rickshaw/rickshaw.min.js";
 import jQuery from "jquery";
 
 window.jQuery = jQuery;
