a5c3b23f8c2c04e70af2431de9348d3e7e1c45ef
Andre Arko <andre@arko.net>
2018-10-02 18:19:15 -0700

    add sidekiq-pro and contribsys source

--- gems.rb
+++ gems.rb
@@ -5,11 +5,20 @@ ruby Pathname.new(".ruby-version").expand_path(__dir__).read
 
 gem "rails", "~> 5.2.0"
 
+gem "aws-sdk-s3", "~> 1.20"
 gem "bootsnap", ">= 1.1.0", require: false
 gem "bulk_insert", "~> 1.7"
 gem "pg", ">= 0.18", "< 2.0"
 gem "puma", "~> 3.11"
+gem "sidekiq", "~> 5.2"
 gem "webpacker", "~> 3.5"
+
+# Gems in both rubygems.org and gems.contribsys.com
+gem "redis"
+gem "connection_pool"
+gem "rack-protection"
+gem "concurrent-ruby"
+gem "rack"
 
 group :development, :test do
   gem "pry-byebug", require: false
@@ -22,6 +31,6 @@ group :development do
   gem "listen", ">= 3.0.5", "< 3.2"
 end
 
-gem "aws-sdk-s3", "~> 1.20"
-
-gem "sidekiq", "~> 5.2"
+source "https://gems.contribsys.com/" do
+  gem "sidekiq-pro"
+end
