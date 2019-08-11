0121d448d5e1f7dcc316057a15b31238eb49d993
Sid Krishnan <krishnan.sid@gmail.com>
2019-08-11 11:38:14 -0400

    remove rickshaw

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,6 @@
 /* eslint no-console:0 */
-import "rickshaw/rickshaw.css";
 import "stylesheets/application.css";
 
-import Rickshaw from "rickshaw/rickshaw.js";
 import jQuery from "jquery";
 import ApexCharts from "apexcharts";
 import Rails from "rails-ujs";
@@ -10,6 +8,5 @@ import Rails from "rails-ujs";
 Rails.start();
 
 window.jQuery = jQuery;
-window.Rickshaw = Rickshaw;
 window.ApexCharts = ApexCharts;
 window.Rails = Rails;
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,9 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || "production";
 
 const environment = require("./environment");
 
-// Rickshaw, even when minified, needs this special variable with this exact name to work
-environment.config.optimization.minimizer[0].options.terserOptions.mangle.reserved = [
-  "$super"
-];
-
 module.exports = environment.toWebpackConfig();
