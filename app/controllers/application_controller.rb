d0667681a9baa6794b11e5c365755883a2f6ab3a
Andre Arko <andre@arko.net>
2021-12-18 23:48:48 -0800

    suppress exception on 404s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,1 +1,5 @@
-class ApplicationController < ActionController::Base; end
+class ApplicationController < ActionController::Base
+  def not_found
+    render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "versions/:key" => "versions#show"
   get "comparison/:key1/:key2" => "comparison#show"
   root "home#index"
+  match "*missing", to: "application#not_found", via: :all
 end
