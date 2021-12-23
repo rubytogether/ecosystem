2c744be7deacbe24b6fd697d4c5926fe3baa8b1f
Andre Arko <andre@arko.net>
2021-12-23 15:43:39 -0700

    render invalid tokens without backtrace log

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActionController::InvalidAuthenticityToken, with: :invalid_token
+
+  def invalid_token
+    render file: Rails.public_path.join('500.html'),
+      status: :unprocessable_entity, layout: false
+  end
+
   def not_found
     render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
   end
