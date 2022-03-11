bbeff99c98cf95531ab8abe26878c89dea19192e
Andre Arko <andre@arko.net>
2022-03-11 03:50:03 -0500

    add sidekiq UI in development

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   get "versions/:key" => "versions#show"
   get "comparison/:key1/:key2" => "comparison#show"
+
+  if Rails.env.development?
+    require "sidekiq/pro/web"
+    mount Sidekiq::Web => "/sidekiq"
+  end
+
   root "home#index"
   match "*missing", to: "application#not_found", via: :all
 end
